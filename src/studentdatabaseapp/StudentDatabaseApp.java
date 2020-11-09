32507df3e9bfe0d005417c39feba7700481e43ac
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2020-11-08 16:16:35 -0800

    Student Database Application v01

--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,5 +1,11 @@
 package studentdatabaseapp;
 
 public class StudentDatabaseApp {
+	
+  public static void main(String[] args) {
+	  student stul = new student();
+	  stul.enroll();
+	  stul.payTuition(500);
+	  
 
-}
+  }}
